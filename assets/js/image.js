d2e4322b7c77b94f5185e9aea9ca7c7e401ea10b
Anar-Qandayev <tu7bxfga8@code.edu.az>
2022-05-19 11:44:59 +0400

    commit

---
+++ assets/js/image.js
@@ -0,0 +1,244 @@
+ countryWithImages = [
+    {
+       "url":"/wiki/%C3%85land_Islands",
+       "alpha3":"ALA",
+       "file_url":"//upload.wikimedia.org/wikipedia/commons/5/52/Flag_of_%C3%85land.svg",
+       "name":"\u00c5land Islands",
+       "license":"Public domain"
+    },
+    {
+       "url":"/wiki/Albania",
+       "alpha3":"ALB",
+       "file_url":"//upload.wikimedia.org/wikipedia/commons/3/36/Flag_of_Albania.svg",
+       "name":"Albania",
+       "license":"Public domain"
+    },
+    {
+       "url":"/wiki/Algeria",
+       "alpha3":"DZA",
+       "file_url":"//upload.wikimedia.org/wikipedia/commons/7/77/Flag_of_Algeria.svg",
+       "name":"Algeria",
+       "license":"Public domain"
+    },
+    {
+       "url":"/wiki/American_Samoa",
+       "alpha3":"ASM",
+       "file_url":"//upload.wikimedia.org/wikipedia/commons/8/87/Flag_of_American_Samoa.svg",
+       "name":"American Samoa",
+       "license":"Public domain"
+    },
+    {
+       "url":"/wiki/Andorra",
+       "alpha3":"AND",
+       "file_url":"//upload.wikimedia.org/wikipedia/commons/1/19/Flag_of_Andorra.svg",
+       "name":"Andorra",
+       "license":"Public domain"
+    },
+    {
+       "url":"/wiki/Angola",
+       "alpha3":"AGO",
+       "file_url":"//upload.wikimedia.org/wikipedia/commons/9/9d/Flag_of_Angola.svg",
+       "name":"Angola",
+       "license":"Public domain"
+    },
+    {
+       "url":"/wiki/Anguilla",
+       "alpha3":"AIA",
+       "file_url":"//upload.wikimedia.org/wikipedia/commons/b/b4/Flag_of_Anguilla.svg",
+       "name":"Anguilla",
+       "license":"Public domain"
+    },
+    {
+       "url":"/wiki/Antigua_and_Barbuda",
+       "alpha3":"ATG",
+       "file_url":"//upload.wikimedia.org/wikipedia/commons/8/89/Flag_of_Antigua_and_Barbuda.svg",
+       "name":"Antigua and Barbuda",
+       "license":"Public domain"
+    },
+    {"file_url":"//upload.wikimedia.org/wikipedia/commons/1/1a/Flag_of_Argentina.svg",
+      "name":"Argentina",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Armenia",
+      "alpha3":"ARM",
+      "file_url":"//upload.wikimedia.org/wikipedia/commons/2/2f/Flag_of_Armenia.svg",
+      "name":"Armenia",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Aruba",
+      "alpha3":"ABW",
+      "file_url":"//upload.wikimedia.org/wikipedia/commons/f/f6/Flag_of_Aruba.svg",
+      "name":"Aruba",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Australia",
+      "alpha3":"AUS",
+      "file_url":"//upload.wikimedia.org/wikipedia/commons/8/88/Flag_of_Australia_%28converted%29.svg",
+      "name":"Australia",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Austria",
+      "alpha3":"AUT",
+      "file_url":"//upload.wikimedia.org/wikipedia/commons/4/41/Flag_of_Austria.svg",
+      "name":"Austria",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Azerbaijan",
+      "alpha3":"AZE",
+      "file_url":"//upload.wikimedia.org/wikipedia/commons/d/dd/Flag_of_Azerbaijan.svg",
+      "name":"Azerbaijan",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Bahamas",
+      "alpha3":"BHS",
+      "file_url":"//upload.wikimedia.org/wikipedia/commons/9/93/Flag_of_the_Bahamas.svg",
+      "name":"Bahamas",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Bahrain",
+      "alpha3":"BHR",
+      "file_url":"//upload.wikimedia.org/wikipedia/commons/2/2c/Flag_of_Bahrain.svg",
+      "name":"Bahrain",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Bangladesh",
+      "alpha3":"BGD",
+      "file_url":"//upload.wikimedia.org/wikipedia/commons/f/f9/Flag_of_Bangladesh.svg",
+      "name":"Bangladesh",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Barbados",
+      "alpha3":"BRB",
+      "file_url":"//upload.wikimedia.org/wikipedia/commons/e/ef/Flag_of_Barbados.svg",
+      "name":"Barbados",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Belarus",
+      "alpha3":"BLR",
+      "file_url":"//upload.wikimedia.org/wikipedia/commons/8/85/Flag_of_Belarus.svg",
+      "name":"Belarus",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Belgium",
+      "alpha3":"BEL",
+      "file_url":"//upload.wikimedia.org/wikipedia/commons/6/65/Flag_of_Belgium.svg",
+      "name":"Belgium",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Belize",
+      "alpha3":"BLZ",
+      "file_url":"//upload.wikimedia.org/wikipedia/commons/e/e7/Flag_of_Belize.svg",
+      "name":"Belize",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Benin",
+      "alpha3":"BEN",
+      "file_url":"//upload.wikimedia.org/wikipedia/commons/0/0a/Flag_of_Benin.svg",
+      "name":"Benin",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Bermuda",
+      "alpha3":"BMU",
+      "file_url":"//upload.wikimedia.org/wikipedia/commons/b/bf/Flag_of_Bermuda.svg",
+      "name":"Bermuda",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Bhutan",
+      "alpha3":"BTN",
+      "file_url":"//upload.wikimedia.org/wikipedia/commons/9/91/Flag_of_Bhutan.svg",
+      "name":"Bhutan",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Bolivia,_Plurinational_State_of",
+      "alpha3":"BOL",
+      "file_url":"//upload.wikimedia.org/wikipedia/commons/d/de/Flag_of_Bolivia_%28state%29.svg",
+      "name":"Bolivia - Plurinational State of",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Bonaire,_Sint_Eustatius_and_Saba",
+      "alpha3":"BES",
+      "file_url":"//upload.wikimedia.org/wikipedia/commons/2/20/Flag_of_the_Netherlands.svg",
+      "name":"Bonaire - Sint Eustatius and Saba",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Bosnia_and_Herzegovina",
+      "alpha3":"BIH",
+      "file_url":"//upload.wikimedia.org/wikipedia/commons/b/bf/Flag_of_Bosnia_and_Herzegovina.svg",
+      "name":"Bosnia and Herzegovina",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Botswana",
+      "alpha3":"BWA",
+      "file_url":"//upload.wikimedia.org/wikipedia/commons/f/fa/Flag_of_Botswana.svg",
+      "name":"Botswana",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Bouvet_Island",
+      "alpha3":"BVT",
+      "file_url":"//upload.wikimedia.org/wikipedia/commons/d/d9/Flag_of_Norway.svg",
+      "name":"Bouvet Island",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Brazil",
+      "alpha3":"BRA",
+      "file_url":"//upload.wikimedia.org/wikipedia/en/0/05/Flag_of_Brazil.svg",
+      "name":"Brazil",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Brunei_Darussalam",
+      "alpha3":"BRN",
+      "file_url":"//upload.wikimedia.org/wikipedia/commons/9/9c/Flag_of_Brunei.svg",
+      "name":"Brunei Darussalam",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Bulgaria",
+      "alpha3":"BGR",
+      "file_url":"//upload.wikimedia.org/wikipedia/commons/9/9a/Flag_of_Bulgaria.svg",
+      "name":"Bulgaria",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Burkina_Faso",
+      "alpha3":"BFA",
+      "file_url":"//upload.wikimedia.org/wikipedia/commons/3/31/Flag_of_Burkina_Faso.svg",
+      "name":"Burkina Faso",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Burundi",
+      "alpha3":"BDI",
+      "file_url":"//upload.wikimedia.org/wikipedia/commons/5/50/Flag_of_Burundi.svg",
+      "name":"Burundi",
+      "license":"Public domain"
+   },
+   {
+      "url":"/wiki/Cambodia",
+      "alpha3":"KHM",
+      "file_url":"//upload.wikimedia.org/wikipedia/commons/8/83/Flag_of_Cambodia.svg",
+      "name":"Cambodia",
+      "license":"Public domain"
+   },
+ ]
---
+++ assets/js/main.js
@@ -0,0 +1,71 @@
+const countryButtonElement = document.querySelector("button");
+countryButtonElement.addEventListener("click", function (amebo) {
+  const randomIndex = Math.floor(Math.random() * countryWithImages.length);
+  const randomIndex1 = Math.floor(Math.random() * countryWithImages.length);
+  const randomIndex2 = Math.floor(Math.random() * countryWithImages.length);
+  const randomIndex3 = Math.floor(Math.random() * countryWithImages.length);
+  const selectedCountry = countryWithImages[randomIndex];
+  window.selectedCountry = selectedCountry;
+  const selectedCountry1 = countryWithImages[randomIndex1];
+  const selectedCountry2 = countryWithImages[randomIndex2];
+  const selectedCountry3 = countryWithImages[randomIndex3];
+  const countries = [
+    selectedCountry,
+    selectedCountry1,
+    selectedCountry2,
+    selectedCountry3,
+  ];
+  let options = document.getElementById("options");
+  let verified = document.getElementById("answer");
+  verified.innerHTML = "";
+  let seconds;
+  let intervaliId;
+  let intViewportWidth
+  let timeRemaining;
+  const gameDuration = 29;
+  function startTimer(seconds) {
+      intervalId = setInterval(countDown, 1000);
+      function countDown() {
+          document.getElementById("timer").innerHTML = `Time: ${seconds}`;
+          if (seconds === 0) {
+              clearInterval(intervalId);
+              finishGame();
+            } else if (seconds === 10) {
+                document.getElementById("timer").style.color = "red";
+                document.getElementById("timer").classList.add("flashing");
+                { seconds = seconds -= 1;
+                }
+            } else {
+                seconds = seconds -= 1;
+            }
+            timeRemaining = seconds;
+        }
+    }
+    options.innerHTML = "";
+    for (let i = 0; i < countries.length; ) {
+        const countryIndex = Math.floor(Math.random() * countries.length);
+        const randomccountry = countries[countryIndex];
+        options.innerHTML += ` <div>
+        <input id="firstOption${countries.length}" onclick="verifyOption(this)" type="radio" name="country" value="${randomccountry.name}"/>
+        <label for="firstOption${countries.length}">${randomccountry.name}</label>
+        </div>`;
+        countries.splice(countryIndex, 1);
+        startTimer(gameDuration);
+    }
+
+document
+.querySelector("img")
+.setAttribute("src", `https:${selectedCountry.file_url}`);
+
+});
+
+const verifyOption = (element) => {
+    let verified = document.getElementById("answer");
+    if (element.value === window.selectedCountry.name) {
+        verified.innerHTML = "CORRECT";
+        verified.style.color = "green";
+    } else {
+        verified.innerHTML = "WRONG";
+        verified.style.color = "red";
+    }
+};
